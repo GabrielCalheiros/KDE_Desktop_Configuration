1ece3f076349f1b5bd32060522352484d28766c2
GabrielCalheirosTP <gabriel.miranda@transpanorama.com.br>
2024-03-25 08:52:20 -0300

    search script

---
+++ jobSearch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+brave --incognito -a "https://www.workana.com/pt" "https://www.99freelas.com.br/" "http://maringa.com/" "https://www.infojobs.com.br/" "https://www.glassdoor.com.br/index.htm" "https://br.indeed.com/" "https://br.linkedin.com/" "https://www.nerdin.com.br/"
